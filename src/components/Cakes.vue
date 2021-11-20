7b583c19c55e56984e5474f1acc0e684c6e10764
fe-lsc <flimascarvalho@gmail.com>
2021-11-20 13:14:57 -0300

    Initial commit

---
+++ src/components/Cakes.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <li v-for=" (cake, index) in cakes" :key="index">
+                {{cake}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            cakes: ['Cheesecake', 'Strawberry', 'Chocolatecake']
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+    li{
+    color: red;
+    }
+</style>
---
+++ src/views/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h2> header </h2>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        background-color: darkgrey;
+        padding: 10px;
+        height: 50px;
+        text-align: center;
+        color: #ffffff;
+    }
+</style>
+
